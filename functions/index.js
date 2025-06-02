e40f23a5504d4351ac8707abd80da26fd7d1a926
jtdeux <125108728+jtdeux@users.noreply.github.com>
2025-06-02 19:34:08 +0800

    Create index.js

---
+++ functions/index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions');
+const fetch = require('node-fetch');
+
+exports.geminiProxy = functions.https.onRequest(async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
+  const apiKey = functions.config().gemini.apikey;
+  try {
+    const response = await fetch('https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + apiKey, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(req.body)
+    });
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } catch (err) {
+    res.status(500).send('Proxy error: ' + err.toString());
+  }
+});
